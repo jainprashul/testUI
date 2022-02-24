c4ffe6ef6e7f65467b7deb6e6fd15470c739a9d7
xpNitro <xpjain11@gmail.com>
2022-02-24 17:41:32 +0530

    added server side validation

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const { validateSignup } = require('../utils');
 
 router.get('/', (req, res) => {
     res.render('signup');
 });
 
-router.post('/signup', (req, res) => {
+router.post('/signup', validateSignup , (req, res) => {
     res.render('postsignup', {
         ...req.body
     });
---
+++ utils/index.js
@@ -0,0 +1,39 @@
+const validate = require('validator');
+
+const validateSignup = (req, res, next) => {
+    const { email, password } = req.body;
+    const phone = req.body.phone;
+    const errors = {};
+
+    if (!email) {
+        errors.email = 'Email is required';
+    } else if (!validate.isEmail(email)) {
+        errors.email = 'Email is invalid';
+    }
+
+    if (!password) {
+        errors.password = 'Password is required';
+    } else if (!validate.isLength(password, { min: 8 })) {
+        errors.password = 'Password must be at least 8 characters';
+    }
+
+    if (phone){
+        if (!validate.isNumeric(phone)) {
+            errors.phone = 'Phone number is invalid \n It should be like: 9123456789';
+        }
+    }
+
+    if (Object.keys(errors).length) {
+        res.render('signup', {
+            errors,
+            ...req.body
+        });
+    } else {
+        next();
+    }
+};
+
+
+module.exports = {
+    validateSignup
+}
