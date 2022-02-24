b393531d7669f604fa865792978c8a0c96e80eb6
xpNitro <xpjain11@gmail.com>
2022-02-24 14:34:08 +0530

    Basic server Done

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-console.log('Hello World');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.set('view engine', 'pug');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('hello world');
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+
+module.exports = app
+
+
 
